faf8ac0120c2fed562a298de9976be55639e85f6
BrunoPCH <63975837+BrunoPCH@users.noreply.github.com>
2021-12-01 14:51:24 -0600

    Delete ARRAYS Y LOOPS directory

--- ARRAYS Y LOOPS/JS/iniciar.js
+++
@@ -1,9 +0,0 @@
-function comienzaJuego() {
-	reparteTarjetas();
-	document.querySelectorAll(".carta").forEach(function(elemento) {
-		elemento.addEventListener("click", descubrir);});
-}
-document.querySelector("#btn_reparte_cartas").addEventListener("click", comienzaJuego);
-window.onload = comienzaJuego();
-
-
--- ARRAYS Y LOOPS/JS/variables-globales.js
+++
@@ -1,3 +0,0 @@
-var grupoTarjetas = ["👩🏻","😘","🎁","🦄","🦋","🐙","🌈","🎆","🌠","🌅","❤️","🐥"];
-
-var totalTarjetas = grupoTarjetas.concat(grupoTarjetas);
